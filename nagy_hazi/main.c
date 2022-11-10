2a519676727b331f3f58a3efef9e0214e13e8141
jdsflk <pergerpatrik113@gmail.com>
2022-11-10 15:15:51 +0100

    Reads the books into linkedList

--- nagy_hazi/main.c
+++ nagy_hazi/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
-Kölcsönzéseket tartalmazó fájl
-- Könyv ID-je (string)
-- Kölcsönző felhasználó ID-je (string)
-- visszaadás határideje (string), formátum YYYY-MM-DD*/
+ * RETURN CODES
+ *      1: couldn't open file
+ *      2: couldn't close file
+ */
 
 typedef struct {
     char title[30];
@@ -13,6 +14,11 @@ typedef struct {
     int publicationDate;
     char bookID[10];
 } book;
+
+typedef struct bookListItem {
+    book data;
+    struct bookListItem *next;
+} bookListItem;
 
 typedef struct {
     char name[30];
@@ -24,7 +30,48 @@ typedef struct {
     book Book;
 } rental;
 
+bookListItem* getBooks(FILE *bfp) {
+    book currentBook;
+    bookListItem *head;
+    bookListItem **tail = &head;
+
+    while (fscanf(bfp, "%s %s %s %d %s", currentBook.title, currentBook.author, currentBook.publisher,
+                  &currentBook.publicationDate, currentBook.bookID) == 5) {
+        bookListItem *temp = (bookListItem*)malloc(sizeof (bookListItem));
+        temp->data= currentBook;
+        temp->next = NULL;
+        *tail =temp;
+        tail= &(*tail)->next;
+    }
+    return head;
+}
+
+int openFiles(FILE *bfp, FILE *ufp, FILE *rfp) {
+    bfp = fopen("C:\\Users\\Asus\\Documents\\Egyetem\\Progi 1\\nagy_hazi\\cmake-build-debug\\b.txt", "r");
+    ufp = fopen("users.txt", "r");
+    rfp = fopen("rentals.txt", "r");
+    if (bfp == NULL || ufp == NULL || rfp == NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+int closeFiles(FILE *bfp, FILE *ufp, FILE *rfp) {
+    if (fclose(bfp) || fclose(rfp) || fclose(ufp)) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    FILE *bookFilePointer = fopen("b.txt", "r");
+    /*if (openFiles(bookFilePointer, userFilePointer, rentalFilePointer) == 1) {
+        return 1;
+    }*/
+    bookListItem* bookListHead = getBooks(bookFilePointer);
+    if (fclose(bookFilePointer)) {
+        return 2;
+    }
+    free(bookListHead);
     return 0;
 }
