de6962fbf521f05154e7d8409c13c0164d1220c8
jdsflk <pergerpatrik113@gmail.com>
2022-12-06 14:31:42 +0100

    insert fixed

--- Labor12/beszuras_faba.c
+++ Labor12/beszuras_faba.c
@@ -5,30 +5,28 @@ typedef struct _node {
     struct _node* left;
     struct _node* right;
 } node;
-
 void insert(node* root, int data) {
     node* currentNode = root;
-    while(currentNode != NULL) {
+    while(currentNode->left != NULL || currentNode->right != NULL) {
+        if(data > currentNode->data) {
+            currentNode = currentNode->right;
+            continue;
+        }
         if(data < currentNode->data) {
             currentNode = currentNode->left;
         }
-        else {
-            currentNode = currentNode->right;
-        }
-        if(data > currentNode->left->data) {
-            break;
-        }
+    }
+    if(data > currentNode->data) {
+        currentNode->right = (node*)malloc(sizeof(node));
+        currentNode->right->right = NULL;
+        currentNode->right->left = NULL;
+        currentNode->right->data = data;
+        return;
     }
     if(data < currentNode->data) {
-        node* prevNext = currentNode->left;
         currentNode->left = (node*)malloc(sizeof(node));
+        currentNode->left->left = NULL;
+        currentNode->left->right = NULL;
         currentNode->left->data = data;
-        currentNode->left->left = prevNext;
-        return;
     }
-    node* prevNext = currentNode->right;
-        currentNode->right = (node*)malloc(sizeof(node));
-        currentNode->right->data = data;
-        currentNode->right->right = prevNext;
-        return;
 }
